a23031136b4606167e7664653ae67c62ad23b105
吕楠 <634237880@qq.com>
2018-01-18 16:54:40 +0800

    第二次代码提交

--- app/src/main/java/com/example/thesecondday/MainActivity.java
+++ app/src/main/java/com/example/thesecondday/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.thesecondday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+    private TextView textView;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            textView = (TextView) findViewById(R.id.asd_text);
+            String string = "马玉强";
+            String string2 = "是傻逼";
+            textView.setText(string+string2);
+            //111
+            Log.e("第一次","---");
+            Log.e("第一次","---");
+            Log.e("第一次","---");
+            Log.e("第一次","---");
+            Log.e("第一次","---");
+            //222
+            Log.e("第二次","---");
+            Log.e("第二次","---");
+            Log.e("第二次","---");
+            Log.e("第二次","---");
+            Log.e("第二次","---");
+            //333
+            Log.e("第三次","---");
+            Log.e("第三次","---");
+            Log.e("第三次","---");
+            Log.e("第三次","---");
+            Log.e("第三次","---");
     }
 }
